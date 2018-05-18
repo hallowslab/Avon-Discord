b6ae29814af6cdedd21d9b80e0dd727f0bc437de
hallowf <alexandre.cortegaca@gmail.com>
2018-05-18 20:36:46 +0100

    Added more basic_commands

--- basic_commands.py
+++ basic_commands.py
@@ -1,9 +1,18 @@
+from ping_host import ping
+
+
 async def commands(message, client):
     if message.content.startswith("!"):
-        if message.author.id == master_id:
-            command = message.content[1:]
-            print(command)
-            if command.upper() == "CLOSE":
-                await client.logout()
-        else:
-            await client.send_message(message.channel , "Fuck off!")
+        command = message.content[1:]
+        if command[:4].upper() == "PING":
+            if command[5:].upper() == "ROCKSTAR":
+                host = "socialclub.rockstargames.com"
+                if ping(host) == True:
+                    await client.send_message(message.channel, "Rockstar social club is online")
+                else:
+                    await client.send_message(message.channel, "Rockstar social club seems to be down")
+            host = command[5:]
+            if ping(host) == True:
+                await client.send_message(message.channel, "Host is online")
+            else:
+                await client.send_message(message.channel, "Host seems to be down, or refusing ping requests")
