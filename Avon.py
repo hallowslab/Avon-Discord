213e6c7edd23f3650082ce497cbd56fd80822b06
hallowf <alexandre.cortegaca@gmail.com>
2018-05-18 18:54:43 +0100

    fixed imports

--- Avon.py
+++ Avon.py
@@ -4,6 +4,7 @@ import commands_test
 from api_key import token
 from authenticated_commands import Commands
 from messages import Responses
+from basic_commands import commands
 
 client = discord.Client()
 
@@ -17,7 +18,7 @@ async def on_ready():
 async def on_message(message):
     await Responses(message, client)
     await Commands(message, client)
-
+    await commands(message, client)
 
 
 client.run(token)
