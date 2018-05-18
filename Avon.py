613ec750040e922b563c4ea4864d5fa70c5374ba
hallowf <alexandre.cortegaca@gmail.com>
2018-05-18 18:51:30 +0100

    Restructured bot

--- Avon.py
+++ Avon.py
@@ -1,38 +1,23 @@
-import discord, asyncio
-from discord.ext import commands
-from discord.ext.commands import Bot
+import discord
+import asyncio
+import commands_test
 from api_key import token
-from api_key import master_id
+from authenticated_commands import Commands
+from messages import Responses
 
-Client = discord.Client()
-bot = Bot(command_prefix = "!")
+client = discord.Client()
 
-@bot.event
+@client.event
 async def on_ready():
-    print("Ready")
+    print("Logged in as")
+    print(client.user.name)
+    print(client.user.id)
 
-@bot.event
+@client.event
 async def on_message(message):
-    # Messages
-    if message.content.upper() == "COOKIE":
-        await bot.send_message(message.channel, ":cookie:")
-    if message.content == "Avon":
-        await bot.send_message(message.channel, "Yes?")
-    if message.content == "avon":
-        await bot.send_message(message.channel, "Yes?")
-    if message.content == "AVON":
-        await bot.send_message(message.channel, "There is no need to yell you cunt")
-        # Commands
-    if message.content.startswith("!Close"):
-        # Command that requires auth
-        if message.author.id == master_id:
-            await bot.close()
-        else:
-            await bot.send_message(message.channel, "Fuck Off!")
-
-@bot.command(pass_context = True)
-async def Close(ctx):
-    await bot.close()
+    await Responses(message, client)
+    await Commands(message, client)
 
 
-bot.run(token)
+
+client.run(token)
