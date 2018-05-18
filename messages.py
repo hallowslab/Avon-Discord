78e3f9df90a59991a228a9bb5836e6290b3de080
hallowf <alexandre.cortegaca@gmail.com>
2018-05-18 18:52:10 +0100

    Added message responses

---
+++ messages.py
@@ -0,0 +1,10 @@
+
+
+
+async def Responses(message, client):
+    if message.content == "Avon":
+        await client.send_message(message.channel, "Yes?")
+    if message.content == "avon":
+        await client.send_message(message.channel, "Yes?")
+    if message.content == "AVON":
+        await client.send_message(message.channel, "There is no need to yell you cunt")
