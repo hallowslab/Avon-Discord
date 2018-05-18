12dc98ecb1a389e6108afc08a7d2a0c21097c74a
hallowf <alexandre.cortegaca@gmail.com>
2018-05-18 20:36:57 +0100

    Added more message responses

--- messages.py
+++ messages.py
@@ -1,3 +1,5 @@
+import datetime
+
 async def Responses(message, client):
     if message.content == "Avon":
         await client.send_message(message.channel, "Yes?")
@@ -5,3 +7,6 @@ async def Responses(message, client):
         await client.send_message(message.channel, "Yes?")
     if message.content == "AVON":
         await client.send_message(message.channel, "There is no need to yell you cunt")
+    if message.content.upper() == "WHAT TIME IS IT?":
+        time = str(datetime.datetime.now().time())
+        await client.send_message(message.channel, "It's: " + time[:8] + " At: UTC")
