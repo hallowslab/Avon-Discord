49095a2a6b32f4ef8e4035b513df680bfb941f0e
hallowf <alexandre.cortegaca@gmail.com>
2018-05-19 17:16:55 +0100

    Updated some messages

--- messages.py
+++ messages.py
@@ -1,12 +1,10 @@
 import datetime
 
 async def Responses(message, client):
-    if message.content == "Avon":
-        await client.send_message(message.channel, "Yes?")
-    if message.content == "avon":
-        await client.send_message(message.channel, "Yes?")
+    if message.content == "Avon" or message.content == "avon":
+        await client.send_message(message.channel, "Yes? {}".format(message.author.mention))
     if message.content == "AVON":
         await client.send_message(message.channel, "There is no need to yell you cunt")
-    if message.content.upper() == "WHAT TIME IS IT?":
+    if message.content.upper() == "WHAT TIME IS IT?" or message.content.upper() == "WHAT TIME IS IT":
         time = str(datetime.datetime.now().time())
         await client.send_message(message.channel, "It's: " + time[:8] + " At: UTC")
