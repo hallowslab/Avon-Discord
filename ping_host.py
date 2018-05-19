44d7e59800c8775bf17e0eb1924f2464bd5f273d
hallowf <alexandre.cortegaca@gmail.com>
2018-05-19 21:24:29 +0100

    Updated ping

--- ping_host.py
+++ ping_host.py
@@ -2,6 +2,6 @@ from platform  import system as system_name
 from subprocess import call as system_call
 
 def ping(host):
-    param = ' -n 1' if system_name().lower()=='windows' else ' -c 1'
-    command = 'ping' + param + " " + host
+    param = ' -n 1 ' if system_name().lower()=='windows' else ' -c 1 '
+    command = 'ping' + param + host
     return system_call(command) == 0
