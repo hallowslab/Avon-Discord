f3ceb9812dfc0caf761fc9d6621ef4c3dc8260bd
hallowf <alexandre.cortegaca@gmail.com>
2018-05-19 17:17:33 +0100

    Added new authenticated command

--- authenticated_commands.py
+++ authenticated_commands.py
@@ -1,11 +1,13 @@
 from api_key import master_id
+from api_key import page_speed
 
 async def Commands(message, client):
     if message.content.startswith("!"):
         if message.author.id == master_id:
             command = message.content[1:]
-            print(command)
-            if command.upper() == "CLOSE":
+            if command.upper().startswith("TESTSPEED"):
+                await client.send_message(message.channel, "Testing speed")
+            if command.upper().startswith("CLOSE"):
                 await client.logout()
         else:
-            await client.send_message(message.channel , "Fuck off!")
+            await client.send_message(message.channel , "You do not have permissions to execute that")
