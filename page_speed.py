33590fd9c0c6243b8171ca64de5417ffc6525407
hallowf <alexandre.cortegaca@gmail.com>
2018-05-20 16:28:45 +0100

    Working on page_speed api

--- page_speed.py
+++ page_speed.py
@@ -1,4 +1,13 @@
+from __future__ import print_function
 from apiclient.discovery import build
-import json
+from httplib2 import Http
+from oauth2client import file, client, tools
+from api_key import page_speed
 
-service = build('pagespeedonline', 'v4', ...)
+service = build('pagespeedonline', 'v4', developerKey = page_speed )
+
+def check_page_speed(page):
+    service.runpagespeed(url=page, utm_campaign=None, screenshot=None, locale=None, rule=None, snapshots=None, strategy=None, filter_third_party_resources=None, utm_source=None)
+
+
+check_page_speed("google.com")
