e1b9a7cd7b0f40a4e932b3aadb901e463cea4bf0
hallowf <alexandre.cortegaca@gmail.com>
2018-05-20 18:33:05 +0100

    Working on ping for linux

--- ping_host.py
+++ ping_host.py
@@ -1,7 +1,9 @@
 from platform  import system as system_name
-from subprocess import call as system_call
+import subprocess
 
 def ping(host):
     param = ' -n 1 ' if system_name().lower()=='windows' else ' -c 1 '
     command = 'ping' + param + host
-    return system_call(command) == 0
+    return subprocess.call(["ping", "-c 1", host]) == 0
+
+ping("google.com")
