b1ed3e386733830bcf03a9950c54ed8da05b2d6e
hallowf <alexandre.cortegaca@gmail.com>
2018-05-20 18:17:40 +0100

    Imported Adv_messages

--- Avon.py
+++ Avon.py
@@ -4,6 +4,7 @@ from api_key import token
 from authenticated_commands import Commands
 from messages import Responses
 from basic_commands import commands
+from Adv_messages import Adv_responses
 
 client = discord.Client()
 
@@ -18,6 +19,7 @@ async def on_message(message):
     await Responses(message, client)
     await Commands(message, client)
     await commands(message, client)
+    await Adv_responses(message, client)
 
 
 client.run(token)
