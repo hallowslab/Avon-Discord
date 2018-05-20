ea1a87bbe561008d3b69366abd1203942acf6ffc
hallowf <alexandre.cortegaca@gmail.com>
2018-05-20 16:29:25 +0100

    Added an array to filter

--- messages.py
+++ messages.py
@@ -1,10 +1,13 @@
 import datetime
 
+
+filter_these = ["FUCK","SHIT","CUNT"]
+
 async def Responses(message, client):
     if message.content == "Avon" or message.content == "avon":
         await client.send_message(message.channel, "Yes? {}".format(message.author.mention))
     if message.content == "AVON":
-        await client.send_message(message.channel, "There is no need to yell you cunt")
+        await client.send_message(message.channel, "There is no need to yell...")
     if message.content.upper() == "WHAT TIME IS IT?" or message.content.upper() == "WHAT TIME IS IT":
         time = str(datetime.datetime.now().time())
         await client.send_message(message.channel, "It's: " + time[:8] + " At: UTC")
