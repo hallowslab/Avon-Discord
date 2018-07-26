6052ed1e482069eaaa27bc4f7b50d36dfa6afa65
hallowf <alexandre.cortegaca@gmail.com>
2018-07-26 14:24:21 +0100

    Added function to play music from url

--- Avon/authenticated_commands.py
+++ Avon/authenticated_commands.py
@@ -1,10 +1,13 @@
-from api_key import master_id
-from api_key import page_speed
+from Avon.api_key import master_id
+from Avon.api_key import speed_api
 import json
+import youtube_dl
 
 
 Music_channel = "446734255561900056"
 playListFolder = "Avon/Music/Playlist.json"
+
+players = {}
 
 def readPlaylist(location):
     with open(location, "r") as f:
@@ -18,11 +21,46 @@ async def Commands(message, client):
             command = message.content[1:]
             if command.upper().startswith("TESTSPEED"):
                 await client.send_message(message.channel, "Testing speed")
-            elif command.upper().startswith("PLAY"):
-                args = command[5:]
-            elif command.upper().startswith("CLOSE"):
+            # # TODO: Create function for queueing songs # FIXME: Update after=lambda: **
+            if command.upper().startswith("PLAY"):
+                Playlist = command[5:]
+                voice_channel = message.author.voice_channel
+                server = message.server
+                if voice_channel == None:
+                    await client.send_message(message.channel, "You don't seem to be connected to any voice channel")
+                    return
+                await client.send_message(message.channel, "Playing song provided in url in {} voice channel".format(voice_channel))
+                #vc = await client.join_voice_channel(voice_channel)
+                #player = await vc.create_ytdl_player(url, after=lambda: ## TODO: Function to run)
+                #players[server.id] = player
+                #player.start()
+            if command.upper().startswith("PLAYURL"):
+                url = command[8:]
+                voice_channel = message.author.voice_channel
+                server = message.server
+                if voice_channel == None:
+                    await client.send_message(message.channel, "You don't seem to be connected to any voice channel")
+                    return
+                await client.send_message(message.channel, "Playing song provided in url in {} voice channel".format(voice_channel))
+                vc = await client.join_voice_channel(voice_channel)
+                player = await vc.create_ytdl_player(url)
+                players[server.id] = player
+                player.start()
+            if command.upper().startswith("PAUSE"):
+                id = message.server.id
+                players[id].pause()
+            if command.upper().startswith("STOPMUSIC"):
+                id = message.server.id
+                players[id].stop()
+            if command.upper().startswith("RESUME"):
+                id = message.server.id
+                players[id].resume()
+            if command.upper().startswith("CLOSE"):
                 await client.logout()
-            else:
-                await client.send_message(message.channel, "Command not recognized, or not implemented...")
+            if command.upper().startswith("DISCONNECT"):
+                server = message.server
+                vc = client.voice_client_in(server)
+                print("disconnecting")
+                await vc.disconnect()
         else:
             await client.send_message(message.channel , "You do not have permissions to execute that")
