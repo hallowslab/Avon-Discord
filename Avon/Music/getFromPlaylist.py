84aded09e071cca1e1d4192f1935cf0d4776d651
hallowf <alexandre.cortegaca@gmail.com>
2018-07-27 16:27:40 +0100

    Updated playlist and script with functions to read playlist

--- Avon/Music/getFromPlaylist.py
+++ Avon/Music/getFromPlaylist.py
@@ -1,17 +1,59 @@
 import json
 
-def readPlaylist(location):
+playListFolder = "Avon/Music/Playlist.json"
+
+def showAllArtists(location=playListFolder):
     with open(location, "r") as f:
         data = json.load(f)
-        return data
+        genres = []
+        artists = []
+        for item in data:
+            genres.append(item)
+        for genre in genres:
+            for index, item in enumerate(data[genre][0]):
+                artists.append(genre + " - " + item)
+        return artists
 
-a = readPlaylist("Avon/Music/Playlist.json")
+def showAllGenres(location=playListFolder):
+    with open(location, "r") as f:
+        data = json.load(f)
+        genres = []
+        for item in data:
+            genres.append(item)
+        return genres
 
-Tuga_all = a["Tuga"][0]
+def readPlaylist(location=playListFolder):
+    with open(location, "r") as f:
+        data = json.load(f)
+        genres = []
+        namesAndUrls = {}
+        for item in data:
+            genres.append(item)
+        for genre in genres:
+            for index, item in enumerate(data[genre][0]):
+                currentArtist = item
+                songsPerArtist = data[genre][0][item]
+                for i in range(len(songsPerArtist)):
+                    songName = songsPerArtist[i]["Name"]
+                    url = songsPerArtist[i]["url"]
+                    namesAndUrls[songName] = url
+        print(namesAndUrls)
+        # for index, item in enumerate(Tuga_all):
+        #     currentArtist = Tuga_all[item]
+        #     for i in range(len(currentArtist)):
+        #         url = currentArtist[i]["url"]
+        #         songName = currentArtist[i]["Name"]
+        #         yield(songName, url)
 
-for index, item in enumerate(Tuga_all):
-    currentArtist = Tuga_all[item]
-    for i in range(len(currentArtist)):
-        url = currentArtist[i]["url"]
-        songName = currentArtist[i]["Name"]
-        print(songName, url)
+def findByGenre(genre, location=playListFolder):
+    with open(location, "r") as f:
+        data = json.load(f)
+        if genre.upper() == "TUGA" or genre.upper() == "PORTUGUES":
+            #Return Tuga music Playlist
+            pass
+        elif genre.upper() == "ENGLISH" or genre.upper() == "ENG":
+            #Return english music playlist
+            pass
+
+
+readPlaylist("Avon/Music/Playlist.json")
