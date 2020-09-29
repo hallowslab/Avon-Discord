087bd06a27167f2e843e4e16cb0c5609567606db
hallowf <alexandre.cortegaca@gmail.com>
2020-09-30 00:07:57 +0100

    Code cleaning and minor fixes/optimizations(Under work)

--- Avon/messages.py
+++ Avon/messages.py
@@ -1,21 +1,18 @@
+import logging
 import datetime
 
-avon_calls = ["Avon", "avon", "Hey avon", "hey avon", "Hey Avon"]
-time_calls = ["WHAT TIME IS IT", "WHAT TIME IS IT?"]
+logger = logging.getLogger("Avon-Discord")
 
+time_calls = ["WHAT TIME IS IT", "WHAT IS THE TIME", "WHAT TIME IT IS"]
 
 async def responses(message, client):
+    """
+    Regular messages and responses
+    """
     if message.author == client.user:
         return
-    ## Explicit messages
-    if message.author == client.user:
-        return
-    if message.content == "AVON":
+    if message.content.upper.startswith() == "AVON":
         await message.channel.send("There is no need to yell...")
-    for x in avon_calls:
-        if message.content == x:
-            await message.channel.send("Yes? {}".format(message.author.mention))
-    for x in time_calls:
-        if message.content.upper() == x:
-            time = str(datetime.datetime.now().time())
-            await message.channel.send("It's: " + time[:8] + " at: UTC")
+    if message.content.upper() in time_calls:
+        time = str(datetime.datetime.now().time())
+        await message.channel.send("It's: " + time[:8] + " at: UTC")
