ce09fa6d2ff0ba793ab268ee139616259cf06c0b
hallowf <alexandre.cortegaca@gmail.com>
2020-09-30 00:04:57 +0100

    Updated confg to have a single function to load all json data

--- Avon/config.py
+++ Avon/config.py
@@ -1,11 +1,17 @@
 import json
 
 access_keys = {}
+settings = {}
 
-def load_keys(store=access_keys):
-  print("Loading keys")
-  with open("Avon/api_keys.json") as j_file:
-    store = json.load(j_file)
-    for API in store:
-      access_keys.update({API: store[API]})
-    
+def load_keys_and_settings():
+    """
+    Load api keys and settings from defined json files
+    """
+    with open("Avon/settings.json") as j_file:
+        j_data = json.load(j_file)
+        for value in j_data:
+            settings.update({value: j_data[value]})
+    with open("Avon/api_keys.json") as j_file:
+        j_data = json.load(j_file)
+        for value in j_data:
+            access_keys.update({value: j_data[value]})
