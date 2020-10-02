94d424666d535257f1beedd91f597ebd1ba2b61c
hallowf <alexandre.cortegaca@gmail.com>
2020-10-02 02:51:22 +0100

    Minor optimization, starting transition to discord.ext.commands framework

--- main.py
+++ main.py
@@ -4,29 +4,41 @@ import logging
 #pylint: disable=unused-import
 import asyncio
 import discord
+from discord.ext import commands
 from Avon import messages, authenticated_commands, profanity_filter, config
 
-client = discord.Client()
+bot = commands.Bot(command_prefix="!")
 logger = logging.getLogger("Avon-Discord")
 
-@client.event
+@bot.event
 async def on_ready():
     """
-    Triggers when the client connects
+    Triggers when the bot connects
     """
-    logger.debug("Logged in as: %s ID: %s", client.user.name, client.user.id)
+    logger.debug("Logged in as: %s ID: %s", bot.user.name, bot.user.id)
 
 
-@client.event
+@bot.event
 async def on_message(message):
     """
     Triggers when a message is sent to the server
     """
-    await messages.responses(message, client)
-    await profanity_filter.filter_messages(message, client)
-    await authenticated_commands.admin_commands(message, client)
-    await authenticated_commands.music_commands(message, client)
-    await authenticated_commands.system_commands(message, client)
+    await messages.responses(message, bot)
+    # Not being used since it has a small limit
+    # await profanity_filter.filter_messages(message, bot)
+    await authenticated_commands.admin_commands(message, bot)
+    await authenticated_commands.music_commands(message, bot)
+    await authenticated_commands.system_commands(message, bot)
+    await bot.process_commands(message)
+
+def set_log_level(level):
+    """
+    Check if the log level is valid and if not sets it to default
+    """
+    n_level = getattr(logging, level.upper(), 20)
+    if n_level == 20 and level.upper() != "INFO":
+        sys.stdout.write("%s: %s\n" % ("Invalid log level", level))
+    return n_level
 
 
 def set_up_logger():
@@ -34,10 +46,9 @@ def set_up_logger():
     Function for setting up the logger and log level
     Also sets up the discord.py logger
     """
+    # Avon-Discord log level from config
     l_level = config.settings["log_level"]
-    n_level = getattr(logging, l_level.upper(), 20)
-    if n_level == 20 and l_level.upper() != "INFO":
-        sys.stdout.write("%s: %s\n" % ("Invalid log level", l_level))
+    n_level = set_log_level(l_level)
 
     # Avon logger
     formatter = logging.Formatter("%(name)s/%(funcName)s - %(levelname)s: %(message)s")
@@ -47,6 +58,7 @@ def set_up_logger():
     stream_handler.setFormatter(formatter)
     logger.addHandler(stream_handler)
     f_handler = logging.FileHandler(filename='Avon.log', encoding='utf-8', mode='w')
+    #pylint: disable=line-too-long
     f_handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s => %(funcName)20s: %(message)s'))
     logger.addHandler(f_handler)
     msg = "%s: %s" % ("Console logger is set with log level", l_level)
@@ -54,18 +66,20 @@ def set_up_logger():
 
     # Discord logger
     dl_level = config.settings["discordpy_log_level"]
-    d_logger = logging.getLogger('discord')
-    d_logger.setLevel(dl_level)
-    stream_handler = logging.StreamHandler()
-    stream_handler.setLevel(n_level)
-    stream_handler.setFormatter(formatter)
-    d_logger.addHandler(stream_handler)
-    f_handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
-    f_handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
-    logger.addHandler(f_handler)
+    if dl_level != "off":
+        n_level = set_log_level(dl_level)
+        d_logger = logging.getLogger('discord')
+        d_logger.setLevel(n_level)
+        stream_handler = logging.StreamHandler()
+        stream_handler.setLevel(n_level)
+        stream_handler.setFormatter(formatter)
+        d_logger.addHandler(stream_handler)
+        f_handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+        f_handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+        logger.addHandler(f_handler)
 
 
 if __name__ == "__main__":
     config.load_keys_and_settings()
     set_up_logger()
-    client.run(config.access_keys["Discord"])
+    bot.run(config.access_keys["discord"])
