43f6c248fe3d1bbbe9ac6dc0dc0bd9754bc48a46
hallowf <alexandre.cortegaca@gmail.com>
2020-10-07 16:45:35 +0100

    Working on music commands

--- Avon/extensions/music_commands.py
+++ Avon/extensions/music_commands.py
@@ -1,5 +1,4 @@
 import logging
-import time
 import asyncio
 from discord.ext import commands
 from Avon import config
@@ -7,118 +6,111 @@ from Avon import config
 
 logger = logging.getLogger("Avon-Discord")
 
-
 async_state = type("", (), {})()
 async_state.music_player = None
-async_state.is_player_busy = False
+async_state.voice_channel = None
 
 
 async def modify_state(state, var, val):
     """
     Modifies the variable in state with the provided value
     """
+    logger.debug("Modifying state: %s => %s", state[var], val)
     state[var] = val
     await asyncio.sleep(0)
     return state
 
 
 @commands.command()
-async def playurl(ctx, url: str, state=async_state):
+@commands.has_any_role("DJ", "Admin")
+async def playurl(ctx, url, state=async_state):
     """
     Plays the specified URL in MUSIC_CHANNEL
     """
-    if str(ctx.message.author.id) == config.access_keys["master_id"]:
-        await modify_state(state, "is_player_busy", True)
-        # FIXME: This is repeated multiple times across this commands,
-        # TODO: Yeet this into another funtion
-        if state.is_player_busy:
-            logger.debug("Music player is busy")
-            await ctx.send("Music player is currently busy")
-            await modify_state(state, "is_player_busy", False)
-            return
-        if state.music_player is not None:
-            logger.debug(state.music_player)
-            await ctx.send("Music player is active use !queue to add a song to the queue")
-            await modify_state(state, "is_player_busy", False)
-            return
-        await ctx.send("Playing url %s in voice channel => %s" % (url))
-        channel_id = config.access_keys["music_channel_id"]
-        logger.debug("Music channel ID => %s", channel_id)
-        channel = ctx.bot.get_channel(channel_id)
-        logger.debug("Channel => %s" % channel)
-        voice_channel = await channel.connect()
-        player = await voice_channel.create_ytdl_player(url)
-        await modify_state(state, "music_player", player)
-        await ctx.send("Playing song provided in url on %s voice channel" % voice_channel)
-        player.start()
-        await modify_state(state, "is_player_busy", False)
-    else:
-        await ctx.send("You don't have permissions to execute that")
+    logger.debug("URL => %s", url)
+    # FIXME: This is repeated multiple times across this commands,
+    # TODO: Yeet this into another funtion
+    if state.music_player is not None:
+        logger.debug(state.music_player)
+        await ctx.send("Music player is active use !queue to add a song to the queue")
+        return
+    channel_id = config.access_keys["music_channel_id"]
+    logger.debug(type(channel_id))
+    await ctx.send("Playing url %s in voice channel => %s" % (url, channel_id))
+    # I still don't know if this works need to test it
+    logger.debug("Music channel ID => %s", channel_id)
+    channel = ctx.bot.get_channel(channel_id)
+    logger.debug("Channel => %s", channel)
+    voice_channel = await channel.connect()
+    await modify_state(state, "voice_channel", voice_channel)
+    player = await voice_channel.create_ytdl_player(url)
+    await modify_state(state, "music_player", player)
+    await ctx.send("Playing song provided in url on %s voice channel" % voice_channel)
+    player.start()
 
 
 @commands.command()
-async def pause(ctx, state=async_state):
-    if str(ctx.message.author.id) == config.access_keys["master_id"]:
-        await modify_state(state, "is_player_busy", True)
-        if state.is_player_busy:
-            logger.debug("Music player is busy")
-            await ctx.send("Music player is currently busy")
-            await modify_state(state, "is_player_busy", False)
-            return
-        if state.music_player is None:
-            logger.debug(state.music_player)
-            await ctx.send("Music player is not active")
-            await modify_state(state, "is_player_busy", False)
-            return
-        await ctx.send("Pausing music")
-        state.music_player.pause()
-    else:
-        await ctx.send("You don't have permissions to execute that")
+@commands.has_any_role("DJ", "Admin")
+async def pause(ctx, arg, state=async_state):
+    """
+    Pauses the music player => can be resumed
+    """
+    logger.debug("Arg => %s", arg)
+    if state.music_player is None:
+        logger.debug(state.music_player)
+        await ctx.send("Music player is not active")
+        return
+    await ctx.send("Pausing music")
+    state.music_player.pause()
 
 
 @commands.command()
+@commands.has_any_role("DJ", "Admin")
 async def stop(ctx, state=async_state):
-    if str(ctx.message.author.id) == config.access_keys["master_id"]:
-        await modify_state(state, "is_player_busy", True)
-        if state.is_player_busy:
-            logger.debug("Music player is busy")
-            await ctx.send("Music player is currently busy")
-            await modify_state(state, "is_player_busy", False)
-            return
-        if state.music_player is None:
-            logger.debug(state.music_player)
-            await ctx.send("Music player is not active")
-            await modify_state(state, "is_player_busy", False)
-            return
-        await ctx.send("Stopping music player")
-        state.music_player.stop()
-        state.music_player = None
-        voice_channel = ctx.bot.get_channel(config.access_keys["music_channel_id"])
-        await ctx.send("Disconnecting")
-        await voice_channel.disconnect()
-        await voice_channel.cleanup()
-    else:
-        await ctx.send("You don't have permissions to execute that")
+    """
+    Pauses the music player => cannot be resumed
+    """
+    if state.music_player is None:
+        logger.debug(state.music_player)
+        await ctx.send("Music player is not active")
+        return
+    await ctx.send("Stopping music player")
+    state.music_player.stop()
+    await modify_state(state, "music_player", None)
+    voice_channel = ctx.bot.get_channel(config.access_keys["music_channel_id"])
+    await ctx.send("Disconnecting")
+    await voice_channel.disconnect()
+    await voice_channel.cleanup()
 
 
 @commands.command()
+@commands.has_any_role("DJ", "Admin")
+async def disconnect(ctx, state=async_state):
+    """
+    Disconnects the bot from the voice channel
+    """
+    if state.voice_channel is None:
+        await ctx.send("Not connected to any voice channel")
+        return
+    await ctx.send("Disconnecting")
+    await state.voice_channel.disconnect()
+    await state.voice_channel.cleanup()
+    await modify_state(state, "voice_channel", None)
+    await modify_state(state, "music_player", None)
+
+
+@commands.command()
+@commands.has_any_role("DJ", "Admin")
 async def resume(ctx, state=async_state):
-    if str(ctx.message.author.id) == config.access_keys["master_id"]:
-        await modify_state(state, "is_player_busy", True)
-        if state.is_player_busy:
-            logger.debug("Music player is busy")
-            await ctx.send("Music player is currently busy")
-            await modify_state(state, "is_player_busy", False)
-            return
-        if state.music_player is None:
-            logger.debug(state.music_player)
-            await ctx.send("Music player is active use !queue to add a song to the queue")
-            await modify_state(state, "is_player_busy", False)
-            return
-        await ctx.send("Resuming music")
-        state.music_player.resume()
-    else:
-        await ctx.send("You don't have permissions to execute that")
+    """
+    Resumes the music player
+    """
+    if state.music_player is None:
+        logger.debug(state.music_player)
+        await ctx.send("Music player is active use !queue to add a song to the queue")
+        return
+    await ctx.send("Resuming music")
+    state.music_player.resume()
 
 
 def setup(bot):
